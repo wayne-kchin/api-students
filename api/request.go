ab32a94a3a94f7c406eccf5b166fe99dea4547dd
Wayne <w4yn3_chin@hotmail.com>
2025-02-12 00:58:05 -0300

    Create student validation

---
+++ api/request.go
@@ -0,0 +1,33 @@
+package api
+
+import "fmt"
+
+type StudentRequest struct {
+	Name   string `json:"name"`
+	CPF    int    `json:"cpf"`
+	Email  string `json:"email"`
+	Age    int    `json:"age"`
+	Active *bool  `json:"active"`
+}
+
+func errParamRequired(param, typ string) error {
+	fmt.Errorf("param '%s' of type '%s' is required", param, typ)
+}
+
+func (s *StudentRequest) Validate() error {
+	if s.Name == "" {
+		return errParamRequired("name", "string")
+	}
+	if s.Email == "" {
+		return errParamRequired("email", "string")
+	}
+	if s.CPF == 0 {
+		return errParamRequired("cpf", "int")
+	}
+	if s.Age == 0 {
+		return errParamRequired("age", "int")
+	}
+	if s.Active == nil {
+		return errParamRequired("active", "bool")
+	}
+}
